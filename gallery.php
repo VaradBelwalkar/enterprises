e8853530a14e99585ca9449914777628c502600a
Varad Belwalkar <varad@kali.example.com>
2023-08-21 00:48:59 +0530

    minor changes

--- gallery.php
+++ gallery.php
@@ -15,6 +15,7 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
   <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/gallery-carousel.css">
 
   <!-- MAIN CSS -->
   <link rel="stylesheet" href="css/templatemo-style.css">
@@ -83,7 +84,7 @@
   </nav>
 
   <!-- Gallary -->
-  <section id="feature">
+  <section id="feature" style="margin-bottom:80px;">
 
   <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel" style=" max-height: 800px;width: 1200px;margin: 0 auto;object-fit: contain;">
     <div class="carousel-indicators">
@@ -105,7 +106,7 @@
         $slideIndex = 0;
         foreach ($imageFiles as $index => $imageFile) {
             if ($imageFile !== '.' && $imageFile !== '..') {
-                echo '<div class="carousel-item ' . ($slideIndex === 0 ? 'active' : '') . '">
+                echo '<div class="carousel-item gallery-image-container ' . ($slideIndex === 0 ? 'active' : '') . '">
                           <img src="' . $imageDirectory . $imageFile . '" class="d-block w-100" alt="Slide ' . ($slideIndex + 1) . '">
                       </div>';
                 $slideIndex++;
@@ -124,79 +125,6 @@
 </div>
 
   </section>
-
-  <br><br>  <br><br>  <br><br>  <br><br>  <br><br>
-  <section>
-    <!-- Carousel wrapper -->
-    <div id="carouselVideoExample" class="carousel slide carousel-fade" data-mdb-ride="carousel">
-      <!-- Indicators -->
-      <div class="carousel-indicators">
-        <button type="button" data-mdb-target="#carouselVideoExample" data-mdb-slide-to="0" class="active"
-          aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-mdb-target="#carouselVideoExample" data-mdb-slide-to="1"
-          aria-label="Slide 2"></button>
-        <button type="button" data-mdb-target="#carouselVideoExample" data-mdb-slide-to="2"
-          aria-label="Slide 3"></button>
-      </div>
-
-      <!-- Inner -->
-      <div class="carousel-inner">
-        <!-- Single item -->
-        <div class="carousel-item active">
-          <video class="img-fluid" autoplay loop muted>
-            <source src="https://mdbcdn.b-cdn.net/img/video/Tropical.mp4" type="video/mp4" />
-          </video>
-          <div class="carousel-caption d-none d-md-block">
-            <h5>First slide label</h5>
-            <p>
-              Nulla vitae elit libero, a pharetra augue mollis interdum.
-            </p>
-          </div>
-        </div>
-
-        <!-- Single item -->
-        <div class="carousel-item">
-          <video class="img-fluid" autoplay loop muted>
-            <source src="https://mdbcdn.b-cdn.net/img/video/forest.mp4" type="video/mp4" />
-          </video>
-          <div class="carousel-caption d-none d-md-block">
-            <h5>Second slide label</h5>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </p>
-          </div>
-        </div>
-
-        <!-- Single item -->
-        <div class="carousel-item">
-          <video class="img-fluid" autoplay loop muted>
-            <source src="https://mdbcdn.b-cdn.net/img/video/Agua-natural.mp4" type="video/mp4" />
-          </video>
-          <div class="carousel-caption d-none d-md-block">
-            <h5>Third slide label</h5>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </div>
-        </div>
-      </div>
-      <!-- Inner -->
-
-      <!-- Controls -->
-      <button class="carousel-control-prev" type="button" data-mdb-target="#carouselVideoExample" data-mdb-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-mdb-target="#carouselVideoExample" data-mdb-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
-    </div>
-    <!-- Carousel wrapper -->
-  </section>
-
-
-
 
   <!-- CONTACT -->
   <section id="contact">
